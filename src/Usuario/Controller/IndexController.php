44eb7751d20d776a99c484b031c1be3ad53c1b28
Cristian Ubilla Villa <cubilla@tbolivar.local>
2016-02-05 16:12:46 -0300

    respalod de paso

---
+++ src/Usuario/Controller/IndexController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Usuario\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use Zend\View\Model\Model;
+use Zend\Db\Adapter\Adapter;
+
+use Zend\Session\Container;
+
+use Sistema\Model\Entity\ReclamoTable;
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    { 
+        $this->layout('layout/usuario');
+    }
+    
+    
+    public function miunidadAction()
+    {        
+        $result = new ViewModel();
+        $result->setTerminal(true);
+       
+        return $result;       
+    }
+    
+    public function micondominioAction()
+    { 
+        $result = new ViewModel();
+        $result->setTerminal(true);
+       
+        return $result;      
+    }
+    
+    public function reclamoAction()
+    {
+            
+            //Instancias
+            $sid = new Container('base');            
+            //Obtenemos datos de sesion                  
+            $db_name = $sid->offsetGet('dbNombre');
+            //Conectamos a BBDD Condominio
+            $this->dbAdapter = $this->getServiceLocator()->get($db_name); 
+            
+            //Obtenemos datos POST
+            $data = $this->request->getPost();
+            
+            //Tablas
+            $reclamo = new ReclamoTable($this->dbAdapter);            
+            $reclamo3 = $reclamo->getReclamo($id);
+                                    
+        //    $this->layout('layout/usuario');         
+
+            return new ViewModel(array('reclamo'=>$reclamo3));
+    }
+}
